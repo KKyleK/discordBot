a0fd23db79bb12442c7688bac04f8419eac4327b
Kyle Koivuneva <KKoivuneva7@gmail.com>
2022-01-07 17:29:23 -0700

    added =j command

--- bot.js
+++ bot.js
@@ -25,15 +25,125 @@ client.on("ready", () => {
 
 //Ping pong test
 client.on("messageCreate", msg => {
-    if (msg.content == "ping") {
-    msg.channel.send("`Pong`");
+    if (msg.content == "i") {
+         msg.channel.send("Games:" + games.length);
+
+         } 
+});
+
+
+
+let current_game = -1;  //Increments as we get new games.
+const games = [];
+let picking = false;
+
+//The Tournament portion: an array of game objects, each with a random id.
+function game(id) {
+
+    this.queue = [];    //All players.
+    this.team_1 = [];   //An array of names.
+    this.team_2 = [];
+    this.id = id;
+}
+
+
+client.on("messageCreate", msg => {
+    if(msg.content == "=j") {
+
+        if (current_game ==-1){  //on start up - make a game.
+            current_game = 0;
+            games[current_game] = new game(getRandomInt(1000));   //Id between 1k
+        }
+        //Concurrent games, make a new game.
+        else if(games[current_game].queue.length == 0 &&  
+            (games[current_game].team_1.length==5 || 
+                games[current_game].team_2.length==5)){ 
+                current_game++; 
+                games[current_game] = new game(getRandomInt(1000));       
+        }
+
+        if(picking){ 
+            console.log("Players currently being picked, wait.");
+        }
+
+        else{     //Add player to game.
+
+            if(!games[current_game].queue.includes(msg.member.displayName)){  //no duplicates
+            games[current_game].queue.push(msg.member.displayName);
+            console.log(msg.member.displayName);
+        }
+        else{
+            console.log(msg.member.displayName + " Is already in the queue.")
+        }
+    }
+
+    if(games[current_game].queue == 10){   //queue pop
+
+        console.log("queue popped!");
+        const captain1_num = getRandomInt(10);
+        let captain2_num = getRandomInt(10);
+
+        while (captain1_num == captain2_num){    //Ensure the captains are different.
+            captain2_num = getRandomInt(10);
+        }
+        captain1 = games[current_game].queue[captain1_num];
+        captain2 = games[current_game].queue[captain2_num];        //These are global too.
+
+        console.log("Captain 1: " + captain1 + " Captain 2: "+ captain2);
+    
+        for (let p of games[current_game].queue){  //Initially print all the players.
+            console.log(p);
+        }
+         current_captain = captain1;  //global
+         next_captain = captain2;
+         picking = true;              //Also global
+    }
+    //END OF =j
+    }
+
+    picking = true; //TEST.
+    if(picking == true) {
+
+
+        const message = msg.content.split(" ");
+        for (let i of message){
+            console.log(i);
+        }
+
+    //Message is: @!id
+    if(message[0] != null && message[0]== "=p" && message.length == 3 && msg.displayName == current_captain) {
+        
+        current_captain = next_captain; 
+        if(next_captain == captain1){ //Get next captain.
+            next_captain = captian2;
+        }
+        else{
+            next_captain = captain1;
+        }
+
+            current_captain = captain2;
+
+            for (let p of games[current_game].queue){  //print remaining players.
+                console.log(p);
+            }
+    }
+
     }
 });
 
 
-const names = [];
-let fill = 0;
-//Add player to a list
+
+
+
+
+
+
+
+
+
+
+
+
 client.on("messageCreate", msg => {
 
     const player = new Player({  
@@ -68,11 +178,7 @@ client.on("messageCreate", msg => {
                 //console.log(JSON.parse(result));
               //  msg.channel.send(result);
             })
-
-
     }
-
-
 });
 
 
